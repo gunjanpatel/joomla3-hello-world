b9728b28d91aa61260d8c883ac14116eff6dfb10
SnehaParmar <sneha1992p@gmail.com>
2014-02-21 18:59:19 +0530

    j3 category component

--- component/admin/sql/install.mysql.utf8.sql
+++ component/admin/sql/install.mysql.utf8.sql
@@ -1,6 +1,4 @@
-DROP TABLE IF EXISTS `#__helloworld`;
-
-CREATE TABLE `#__helloworld` (
+CREATE TABLE IF NOT EXISTS `#__helloworld` (
 	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
 	`greeting` VARCHAR(25) NOT NULL,
 	`published` tinyint(4) NOT NULL,
@@ -10,6 +8,24 @@ CREATE TABLE `#__helloworld` (
 	AUTO_INCREMENT =0
 	DEFAULT CHARSET =utf8;
 
-INSERT INTO `#__helloworld` (`greeting`) VALUES
-('Hello World!'),
-('Good bye World!');
+CREATE TABLE IF NOT EXISTS `#__helloworld_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `lft` int(11) NOT NULL DEFAULT '0',
+  `rgt` int(11) NOT NULL DEFAULT '0',
+  `level` int(10) unsigned NOT NULL DEFAULT '0',
+  `title` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `alias` varchar(255) NOT NULL DEFAULT '',
+  `access` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `path` varchar(255) NOT NULL DEFAULT '',
+  `published` int(1) NOT NULL DEFAULT '0',
+  `checked_out` int(4) NOT NULL DEFAULT '0',
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_user_id` int(4) NOT NULL DEFAULT '875',
+  PRIMARY KEY (`id`),
+  KEY `idx_left_right` (`lft`,`rgt`)
+)
+  ENGINE=MyISAM
+  DEFAULT CHARSET=utf8
+  COMMENT="Store helloworld category data";
